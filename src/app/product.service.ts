e6c6b00f8fccf734a9fd22a2b17a1912dc6b114e
lawrence-chege <lawrence.chege@meltwater.org>
2019-11-08 20:28:01 +0000

    fix import error

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,14 +8,13 @@ import { Observable } from 'rxjs/Observable';
 
 import { Album } from './album';
 
-import { Product } from '../product';
 import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   
   private _albumUrl = '../assets/album.json';
-  private _productUrl = '../assets/products.json'
+  private _productsUrl = '../assets/products.json'
 
   constructor(private _http: Http) { }
 
@@ -25,7 +24,7 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]>{
-    return this._http.get(this._productUrl).map((response)=>
+    return this._http.get(this._productsUrl).map((response)=>
     <Product[]>response.json());
   }
 
